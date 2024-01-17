4e9108725d1fa4dce37d9d94722ccea5b9d6819a
Augusto Moscardo <augustomsaluz@gmail.com>
2024-01-16 23:09:21 -0300

    fix folder path to connect with db

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'database', 'database.db')
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
     useNullAsDefault: true,
     pool: {
