695c7a19fc523fb6a5a15b5b308d96cf11b21df7
Augusto Moscardo <augusto.almeida@redeoba.com.br>
2024-01-16 22:30:23 -0300

    add users migration with knex

--- src/database/knex/migrations/20240116023859_createUsers.js
+++ src/database/knex/migrations/20240116023859_createUsers.js
@@ -4,9 +4,12 @@
  */
 exports.up = knex => knex.schema.createTable("users", table => {
   table.increments("id")
-  table.text("name")
-  table.text("email")
-  table.text("password")
+  table.text("name").notNullable()
+  table.text("email").notNullable()
+  table.text("password").notNullable()
+  table
+    .enum("role", ["admin", "user"], { useNative: true, enumName: "roles" })
+    .notNullable().default("customer")
 
   table.timestamp("created_at").default(knex.fn.now)
   table.timestamp("uodated_at").default(knex.fn.now)
